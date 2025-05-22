4bb5fad048f8d1a8f1ebb98b923a76aa56c48704
gojo766j <sayuramihiranga26@gmail.com>
2025-05-22 18:24:42 +0530

    Update setting.js

--- plugins/setting.js
+++ plugins/setting.js
@@ -78,7 +78,7 @@ async (conn, mek, m, { from, isOwner, quoted, reply }) => {
 
 > 𝐏ᴏᴡᴇʀᴅ 𝐁ʏ 𝐆𝐎𝐉𝐎_𝐌ᴅ......👁️❗`;
 
-        const vv = await conn.sendMessage(from, { image: { url: ""}, caption: desc }, { quoted: mek });
+        const vv = await conn.sendMessage(from, { image: { url: "https://raw.githubusercontent.com/gojo766j/GOJO-HELPER/refs/heads/main/file_00000000d0dc61f597f450261ecfe33f%20(1).png"}, caption: desc }, { quoted: mek });
 
         conn.ev.on('messages.upsert', async (msgUpdate) => {
             const msg = msgUpdate.messages[0];
