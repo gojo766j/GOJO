027a6dadfad3eb5c5683c76cf44205d1b21851e4
gojo766j <sayuramihiranga26@gmail.com>
2025-05-22 15:34:00 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,14 +13,14 @@ cmd({
     try {
         // Send image + caption
         await gojo.sendMessage(from, {
-            image: { url: "https://raw.githubusercontent.com/Gojo899/Bot-photo-and-video-/refs/heads/main/Photo/file_00000000d0dc61f597f450261ecfe33f%20(1).png" },
+            image: { url: "photo" },
             caption: `⚡ GOJO MAX is ALIVE ⚡\n\nSystem Status: ONLINE ✅\nBot Power Level: ∞\n\nCreated & Managed by: sayura\n\nType .menu to explore commands!`
         }, { quoted: mek });
 
         // Send voice message (PTT style)
         await gojo.sendMessage(from, {
             audio: {
-                url: "https://github.com/Gojo899/Bot-photo-and-video-/raw/refs/heads/main/mp3/1747630063493150813rf60n6vl-voicemaker.in-speech.mp3"
+                url: "mp3"
             },
             mimetype: 'audio/mpeg',
             ptt: true
