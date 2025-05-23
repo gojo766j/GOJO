fcac59533bf8907216d19424fbe653a3f18bff4b
gojo766j <sayuramihiranga26@gmail.com>
2025-05-24 02:25:35 +0530

    Create scraper.js

---
+++ 2 plugins/scraper.js
@@ -0,0 +1,14 @@
+[{
+  "url": "https://sinhalasub.lk/",
+  "pageTitle": "SinhalaSub - සිංහල උපසිරසි සමඟ ඔන්ලයින් බලන්න ඩිරෙක්ට් ඩවුලෝඩ් කරන්න",
+  "h1": "",
+  "first_h2": "Movies",
+  "random_text_from_the_page": ""
+},
+{
+  "url": "http://www.example.com",
+  "pageTitle": "Example Domain",
+  "h1": "Example Domain",
+  "first_h2": "",
+  "random_text_from_the_page": "This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission."
+}]
