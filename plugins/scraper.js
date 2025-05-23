d1b504a81ed5eb4b2bfa633948695f87e37a90c4
gojo766j <sayuramihiranga26@gmail.com>
2025-05-24 01:41:14 +0530

    Update scraper.js

--- plugins/scraper.js
+++ plugins/scraper.js
@@ -2,22 +2,25 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function getMovieByName(query) {
-    const url = `https://sinhalasub.lk/?s=${encodeURIComponent(query)}`;
-    const res = await axios.get(url);
-    const $ = cheerio.load(res.data);
-    const posts = $(".post");
+    try {
+        const url = `https://sinhalasub.lk/?s=${encodeURIComponent(query)}`;
+        const res = await axios.get(url);
+        const $ = cheerio.load(res.data);
+        const movies = [];
 
-    const movies = [];
+        $(".post").each((i, el) => {
+            const title = $(el).find(".post-title").text().trim();
+            const link = $(el).find("a").attr("href");
+            if (title && link) {
+                movies.push({ title, link });
+            }
+        });
 
-    posts.each((i, el) => {
-        const title = $(el).find(".post-title").text().trim();
-        const link = $(el).find("a").attr("href");
-        if (title && link) {
-            movies.push({ title, link });
-        }
-    });
-
-    return movies;
+        return movies;
+    } catch (err) {
+        console.error("Scraper error:", err.message);
+        return [];
+    }
 }
 
 module.exports = { getMovieByName };
