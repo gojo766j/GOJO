be1cc1d2d85fe70194a0395aff0da2f1994ba2b1
gojo766j <sayuramihiranga26@gmail.com>
2025-05-24 01:39:14 +0530

    Update scraper.js

--- plugins/scraper.js
+++ plugins/scraper.js
@@ -1,28 +1,23 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function getMovieByName(movieName) {
-  const baseUrl = "https://sinhalasub.lk";
-  const searchUrl = `${baseUrl}/?s=${encodeURIComponent(movieName)}`;
-
-  try {
-    const res = await axios.get(searchUrl);
+async function getMovieByName(query) {
+    const url = `https://sinhalasub.lk/?s=${encodeURIComponent(query)}`;
+    const res = await axios.get(url);
     const $ = cheerio.load(res.data);
+    const posts = $(".post");
 
-    const results = [];
-    $(".post").each((i, el) => {
-      const title = $(el).find(".post-title").text().trim();
-      const link = $(el).find("a").attr("href");
-      if (title.toLowerCase().includes(movieName.toLowerCase())) {
-        results.push({ title, link });
-      }
+    const movies = [];
+
+    posts.each((i, el) => {
+        const title = $(el).find(".post-title").text().trim();
+        const link = $(el).find("a").attr("href");
+        if (title && link) {
+            movies.push({ title, link });
+        }
     });
 
-    return results;
-  } catch (error) {
-    console.error("Error fetching movie:", error.message);
-    return [];
-  }
+    return movies;
 }
 
 module.exports = { getMovieByName };
