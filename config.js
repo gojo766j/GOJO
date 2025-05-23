9797f2fcda16ac5f0ec8b42792d0ea84f8c5c2e6
gojo766j <sayuramihiranga26@gmail.com>
2025-05-23 12:13:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "0iFHWLya#wUrewaj_goLU4kqQJ7HZbe-KaIqGEg5o_FKCBB-BE_k",
+  SESSION_ID: process.env.SESSION_ID || "V3dTHCQJ#-LvaDvnuhGqSt2t5SpAYLN6Kt7C4Xx2qMZCYJGMpWlY",
   OWNER_NUM: process.env.OWNER_NUM || "94743826406",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
